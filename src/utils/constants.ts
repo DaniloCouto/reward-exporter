cb5e5783cf8e9fbb441e07abc5e00b5f432ca055
Danilo Soares Couto <danilo.s.couto@gmail.com>
2023-04-21 17:40:30 -0300

    Updates

--- src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const APP_CLIENT_ID = "yz455msrl2ph2v97dg4csppqs89cnk"
-export const REDIRECT_URL = 'http://localhost:3000/rewards'
-export const SCOPE_CHANNEL_REWARDS = 'channel:read:redemptions'
-
-export default {
-    APP_CLIENT_ID,
-    REDIRECT_URL,
-    SCOPE_CHANNEL_REWARDS
-}
